b7788b89e4e2846cc3f969aa2e5610262590101e
RamintaRam <raminta5@yahoo.com>
2017-05-18 14:49:21 +0300

    blah

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -27,7 +27,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        $schedule->command('view:id')->everyMinute();
+        $schedule->command('view:id')->monthly();
     }
 
     /**
